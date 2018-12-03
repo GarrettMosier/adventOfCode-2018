35925b6c4eef9f358e376723d66f9383c419201d
Garrett Mosier <garrettmosier@gmail.com>
2018-12-03 11:41:49 -0800

    Formatted answer for day three

--- dayThree.hs
+++ dayThree.hs
@@ -12,10 +12,8 @@ main = do
   let splitInput = lines input
   let fabricLines = fmap (parse parser "") splitInput
   let validFabrics = fmap fromRight fabricLines
-  print $ countOverlaps validFabrics
-  print $ findSolo validFabrics
-
-inputLocation = "../Downloads/input"
+  print ("The answer to part one is " ++ (show (countOverlaps validFabrics)))
+  print ("The answer to part two is " ++ (show (lineNo $ findSolo validFabrics)))
 
 type Coord = (Int, Int)
 
@@ -26,8 +24,10 @@ data Fabric = Fabric {
   width :: Int,
   height :: Int } deriving Show
 
+
 fabricToCoords :: Fabric -> [Coord]
 fabricToCoords (Fabric line left top wi hei) = [(x,y) | x <- [left+1 .. left + wi], y <- [top+1 .. top + hei]]
+
 
 countOverlaps :: [Fabric] -> Int
 countOverlaps fabrics = length $ Data.List.filter (\x -> x >= 2) $ fmap snd $ getCoordCounts fabrics
@@ -37,6 +37,7 @@ findSolo :: [Fabric] -> Fabric
 findSolo fabrics = head $ take 1 $ Data.List.filter (\x -> subset (fabricToCoords x) onceUsedSpots) fabrics
   where onceUsedSpots = fmap fst $ Data.List.filter (\x -> snd x == 1) $ getCoordCounts fabrics :: [Coord]
 
+
 subset xs ys  = all (`elem` ys) xs 
 
 getCoordCounts :: [Fabric] -> [(Coord, Int)]
@@ -44,6 +45,7 @@ getCoordCounts fabrics = coordCounts
   where coords = concat $ fmap fabricToCoords fabrics
         groupedCoords = (group . sort) coords 
         coordCounts = fmap (\x -> (x !! 0, length x)) groupedCoords 
+
 
 int :: Parser Int
 int = read <$> many1 digit
@@ -64,3 +66,6 @@ parser = do
   _ <- Text.Parsec.oneOf "x"
   height <- int
   return (Fabric line left top width height)
+
+
+inputLocation = "../Downloads/input-3.txt"
