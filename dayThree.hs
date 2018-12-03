128f8f51a3bd318b79a70843581e8571ff275011
Garrett Mosier <garrettmosier@gmail.com>
2018-12-03 07:30:13 -0800

    Finished day three

--- dayThree.hs
+++ dayThree.hs
@@ -11,27 +11,39 @@ main = do
   input <- readFile inputLocation
   let splitInput = lines input
   let fabricLines = fmap (parse parser "") splitInput
-  print fabricLines
-  print $ countOverlaps $ fmap fromRight fabricLines
+  let validFabrics = fmap fromRight fabricLines
+  print $ countOverlaps validFabrics
+  print $ findSolo validFabrics
 
 inputLocation = "../Downloads/input"
 
 type Coord = (Int, Int)
 
 data Fabric = Fabric {
+  lineNo :: Int,
   leftOffset :: Int,
   topOffset :: Int,
   width :: Int,
   height :: Int } deriving Show
 
 fabricToCoords :: Fabric -> [Coord]
-fabricToCoords (Fabric left top wi hei) = [(x,y) | x <- [left+1 .. left + wi], y <- [top+1 .. top + hei]]
+fabricToCoords (Fabric line left top wi hei) = [(x,y) | x <- [left+1 .. left + wi], y <- [top+1 .. top + hei]]
 
 countOverlaps :: [Fabric] -> Int
-countOverlaps fabrics = length $ Data.List.filter (\x -> x >= 2) coordCounts
+countOverlaps fabrics = length $ Data.List.filter (\x -> x >= 2) $ fmap snd $ getCoordCounts fabrics
+
+
+findSolo :: [Fabric] -> Fabric
+findSolo fabrics = head $ take 1 $ Data.List.filter (\x -> subset (fabricToCoords x) onceUsedSpots) fabrics
+  where onceUsedSpots = fmap fst $ Data.List.filter (\x -> snd x == 1) $ getCoordCounts fabrics :: [Coord]
+
+subset xs ys  = all (`elem` ys) xs 
+
+getCoordCounts :: [Fabric] -> [(Coord, Int)]
+getCoordCounts fabrics = coordCounts 
   where coords = concat $ fmap fabricToCoords fabrics
         groupedCoords = (group . sort) coords 
-        coordCounts = fmap length groupedCoords 
+        coordCounts = fmap (\x -> (x !! 0, length x)) groupedCoords 
 
 int :: Parser Int
 int = read <$> many1 digit
@@ -39,7 +51,7 @@ int = read <$> many1 digit
 parser :: Parser Fabric
 parser = do
   _ <- Text.Parsec.oneOf "#"
-  _ <- int
+  line <- int
   _ <- Text.Parsec.oneOf " "
   _ <- Text.Parsec.oneOf "@"
   _ <- Text.Parsec.oneOf " "
@@ -51,16 +63,4 @@ parser = do
   width <- int
   _ <- Text.Parsec.oneOf "x"
   height <- int
-  return (Fabric left top width height)
-  
-
-
-
-{-
-parseFabric :: String -> Coord
-parseFabric x = 
-  where dimensions = splitOn "@" x
-        otherDimensions = splitOn ":" dimensions
-        offsets = split "," (otherDimensions !! 0)
-        position = split ":" (otherDimensions !! 1)
--}
+  return (Fabric line left top width height)
