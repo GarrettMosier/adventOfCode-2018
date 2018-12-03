bd55d1b4266b4618b38772327f0070c04a16a5b9
Garrett Mosier <garrettmosier@gmail.com>
2018-12-02 23:14:52 -0800

    Passed part one of day three

--- dayThree.hs
+++ dayThree.hs
@@ -1,12 +1,18 @@
 import Data.List.Split
 import Data.List
 import Data.Set 
+import Data.Either.Unwrap
+
+import Text.Parsec
+import Text.Parsec.String
 
 
 main = do
   input <- readFile inputLocation
-  let splitInput = splitOn "\n" input
-  print splitInput
+  let splitInput = lines input
+  let fabricLines = fmap (parse parser "") splitInput
+  print fabricLines
+  print $ countOverlaps $ fmap fromRight fabricLines
 
 inputLocation = "../Downloads/input"
 
@@ -19,13 +25,36 @@ data Fabric = Fabric {
   height :: Int } deriving Show
 
 fabricToCoords :: Fabric -> [Coord]
-fabricToCoords (Fabric left top wi hei) = [(x,y) | x <- [left .. left + wi], y <- [top .. top + hei]]
+fabricToCoords (Fabric left top wi hei) = [(x,y) | x <- [left+1 .. left + wi], y <- [top+1 .. top + hei]]
 
 countOverlaps :: [Fabric] -> Int
 countOverlaps fabrics = length $ Data.List.filter (\x -> x >= 2) coordCounts
   where coords = concat $ fmap fabricToCoords fabrics
         groupedCoords = (group . sort) coords 
         coordCounts = fmap length groupedCoords 
+
+int :: Parser Int
+int = read <$> many1 digit
+
+parser :: Parser Fabric
+parser = do
+  _ <- Text.Parsec.oneOf "#"
+  _ <- int
+  _ <- Text.Parsec.oneOf " "
+  _ <- Text.Parsec.oneOf "@"
+  _ <- Text.Parsec.oneOf " "
+  left <- int
+  _ <- Text.Parsec.oneOf ","
+  top <- int
+  _ <- Text.Parsec.oneOf ":" -- TODO Learn how to have it ignore the entire string, not just the one character
+  _ <- Text.Parsec.oneOf " "
+  width <- int
+  _ <- Text.Parsec.oneOf "x"
+  height <- int
+  return (Fabric left top width height)
+  
+
+
 
 {-
 parseFabric :: String -> Coord
