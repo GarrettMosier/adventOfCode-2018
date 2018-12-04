67b39b57905cad1856794c39dd6b377f5b7cc666
Garrett Mosier <garrettmosier@gmail.com>
2018-12-03 21:32:09 -0800

    Sorted timestamps and made Action type

--- dayFour.hs
+++ dayFour.hs
@@ -1,13 +1,21 @@
 import Text.Parsec
 import Text.Parsec.String
 
+import Data.List
+
+import Data.Either
+
+import Data.Map
+
 import Common
 
 main = do
   input <- fmap lines $ readFile inputLocation
-  print $ fmap (parse parser "") input
+  let sortedActions = sort $ rights $ fmap (parse parser "") input
+  mapM_ print $ take 10 $ sortedActions
   print ("The solution to part one is " ++ "")
   print ("The solution to part two is " ++ "")
+
 
 data Timestamp = Timestamp {
   year :: Int,
@@ -15,7 +23,19 @@ data Timestamp = Timestamp {
   day :: Int,
   hour :: Int,
   minute :: Int,
-  action :: String } deriving Show
+  action :: String } deriving (Show, Ord, Eq) -- Make it have the Action type
+
+data Action = BeginShift GuardID | FallAsleep | WakeUp 
+
+type GuardID = Int
+
+
+calculateSleepyGuard :: [Timestamp] -> GuardID
+calculateSleepyGuard x = calculateSleepyGuardHelper x empty
+
+
+calculateSleepyGuardHelper :: [Timestamp] -> Map GuardID Int -> GuardID
+calculateSleepyGuardHelper line = undefined
 
 
 parser :: Parser Timestamp
